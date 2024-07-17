21c3e80c0ee40c47ebae09d33800dc34ba1686c0
Shamshiyev Og'abek <shamshiyevogabek5555@gmail.com>
2024-07-17 18:11:59 +0500

    add

--- app.js
+++ app.js
@@ -1,23 +1,3 @@
-// function getCurrentFormattedTime(template) {
-//   let date = new Date();
-
-//   let MM = String(date.getMinutes()).padStart(2, "0");
-//   let SS = String(date.getSeconds()).padStart(2, "0");
-//   let HH = String(date.getHours()).padStart(2, "0");
-//   let YY = String(date.getFullYear());
-
-//   let formattedTime = template
-//     .replace("${MM}", MM)
-//     .replace("${SS}", SS)
-//     .replace("${HH}", HH)
-//     .replace("${YY}", YY);
-
-//   return formattedTime;
-// }
-
-// let template = "${MM}.${SS}.${HH}.${YY}";
-// console.log(getCurrentFormattedTime(template));
-
 const birthDateInput = prompt(
   "Tug'ilgan kuningizni kiriting (masalan, 01.11.2011 yoki 2011-11-01)"
 );
